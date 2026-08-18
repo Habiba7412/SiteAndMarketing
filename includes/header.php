6939cab73d5e4bc6a87284c500c07830e930e5be
habiba7412 <digiraremarketing@gmail.com>
2026-08-18 17:15:47 +0500

    Add Google Site Verification tag

--- includes/header.php
+++ includes/header.php
@@ -45,6 +45,7 @@ $currentPage = basename($_SERVER['SCRIPT_NAME']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="Vn-Xz3Ig7mUJtVNwhK8QX-T2E8fl3KTEKKx4U0gi0lo" />
     
     <?php 
     // Render 100% Database-Driven Meta, OpenGraph, Twitter Cards, Schema, & Tracking Scripts
